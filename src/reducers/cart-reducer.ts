173749326c024b7cbd072c3b01447645a1db234c
Enrique Comesaña <enriquecbar@gmail.com>
2024-04-28 16:10:33 +0200

    fin reducer + localStorage

--- src/reducers/cart-reducer.ts
+++ src/reducers/cart-reducer.ts
@@ -4,11 +4,11 @@ import { CartItem, Guitar } from '../types';
 export type CartActions =
   | {
       type: 'add-to-cart';
-      payload: { Item: Guitar };
+      payload: { item: Guitar };
     }
-  | { type: 'remove-from-cart'; payload: { Id: Guitar['id'] } }
-  | { type: 'decrease-quantity'; payload: { Id: Guitar['id'] } }
-  | { type: 'increase-quantity'; payload: { Id: Guitar['id'] } }
+  | { type: 'remove-from-cart'; payload: { id: Guitar['id'] } }
+  | { type: 'decrease-quantity'; payload: { id: Guitar['id'] } }
+  | { type: 'increase-quantity'; payload: { id: Guitar['id'] } }
   | { type: 'clear-cart' };
 
 export interface CartState {
@@ -16,42 +16,108 @@ export interface CartState {
   cart: CartItem[];
 }
 
+const initialCart = (): CartItem[] => {
+  const localStorageCart = localStorage.getItem('cart');
+  return localStorageCart ? JSON.parse(localStorageCart) : [];
+};
+
+
+
 export const initialState: CartState = {
   data: db,
-  cart: []
+  cart: initialCart()
 };
+
+const MIN_ITEMS = 1;
+const MAX_ITEMS = 5;
 
 export const cartReducer = (
   state: CartState = initialState,
   action: CartActions
 ) => {
   if (action.type === 'add-to-cart') {
+    const itemExists = state.cart.find(
+      guitar => guitar.id === action.payload.item.id
+    );
+
+    let updatedCart: CartItem[] = [];
+
+    if (itemExists) {
+      // existe en el carrito
+      updatedCart = state.cart.map(item => {
+        if (item.id === action.payload.item.id) {
+          if (item.quantity < MAX_ITEMS) {
+            return {
+              ...item,
+              quantity: item.quantity + 1
+            };
+          } else {
+            return item;
+          }
+        } else {
+          return item;
+        }
+      });
+    } else {
+      const newItem: CartItem = { ...action.payload.item, quantity: 1 };
+      updatedCart = [...state.cart, newItem];
+    }
+
     return {
       ...state,
-      cart: [...state.cart, action.payload.Item]
+      cart: updatedCart
     };
   }
 
   if (action.type === 'remove-from-cart') {
+    const updatedCart = state.cart.filter(
+      item => item.id !== action.payload.id
+    );
     return {
-      ...state
+      ...state,
+      cart: updatedCart
     };
   }
 
   if (action.type === 'decrease-quantity') {
+    const cart = state.cart.map(item => {
+      if (item.id === action.payload.id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        };
+      }
+      return item;
+    });
     return {
-      ...state
+      ...state,
+      cart
     };
   }
+
+
+
   if (action.type === 'increase-quantity') {
+    const cart = state.cart.map(item => {
+      if (item.id === action.payload.id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        };
+      }
+      return item;
+    });
+
     return {
-      ...state
+      ...state,
+      cart
     };
   }
 
   if (action.type === 'clear-cart') {
     return {
-      ...state
+      ...state,
+      cart: []
     };
   }
 };
